61fad5e07b8f781f3a23fa9ec24c2cbcbf382439
ssule <ssulecelepp@gmail.com>
2024-01-27 15:16:16 +0300

    Environment tamamlandı.

---
+++ Environment/UdemyEnvironment/Controllers/HomeController.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+using UdemyEnvironment.Models;
+
+namespace UdemyEnvironment.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+        //www.tenohub.com
+        //www.alfa.teknohub.com
+        public IActionResult Index()
+        {
+            //throw new Exception("Hata var");
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
